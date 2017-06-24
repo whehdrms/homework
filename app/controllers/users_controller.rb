1d2b0ee8609a3e0bdabb6fe62f1c68f412ee8e8a
whehdrms <d1g1mong@naver.com>
2017-06-24 10:01:36 +0000

    login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,14 +11,19 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
       render 'new'
     end
   end
-
-  private
+  # Logs out the current user.
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
+end
 
     def user_params
       params.require(:user).permit(:name, :email, :password,
